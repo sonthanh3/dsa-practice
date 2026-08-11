a022afef85fa7385913c40237070b2f6b192bfc8
Son <son.tnguyen97@gmail.com>
2026-08-10 21:10:44 -0400

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+    public String serialize(TreeNode root) {
+        if (root == null) return "#";
+        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+    }
+
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return helper(queue);
+    }
+    
+    private TreeNode helper(Queue<String> queue) {
+        String s = queue.poll();
+        if (s.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.valueOf(s));
+        root.left = helper(queue);
+        root.right = helper(queue);
+        return root;
+    }
+}
