920f149e4fe1d081e5cee43c6f1b54951dc53f40
Son <son.tnguyen97@gmail.com>
2026-09-01 23:44:54 -0400

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public void reverseString(char[] s) {
+        List<Character> list = new ArrayList<>();
+        for (char c : s) {
+            list.add(c);
+        }
+        Collections.reverse(list);
+
+        for (int i = 0; i < s.length; i++) {
+            s[i] = list.get(i);
+        }
+    }
+}
